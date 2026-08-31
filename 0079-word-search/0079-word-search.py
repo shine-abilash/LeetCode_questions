6eded6827f44d523e93a59eb0b847e651b315576
shine-abilash <shineabilash06@gmail.com>
2026-08-31 10:19:47 +0530

    Time: 3479 ms (65.03%), Space: 19.5 MB (28.2%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -2,28 +2,29 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         row=len(board)
         col=len(board[0])
-
-        def dfs(x,y,i):
-            if i==len(word):
+        sol=[[0]*col for i in range(row)]
+        def find(x,y,ind):
+            if ind==len(word):
                 return True
-            if x<0 or y<0 or x>=row or y>=col or board[x][y]!=word[i] or sol[x][y]==1:
+            if x<0 or y<0 or x>=row or y>=col:
+                return False
+            if board[x][y]!=word[ind] or sol[x][y]==1:
                 return False
             sol[x][y]=1
-            if dfs(x+1,y,i+1):
+            if find(x+1,y,ind+1):
                 return True
-            if dfs(x,y+1,i+1):
+            if find(x,y+1,ind+1):
                 return True
-            if dfs(x-1,y,i+1):
+            if find(x-1,y,ind+1):
                 return True
-            if dfs(x,y-1,i+1):
+            if find(x,y-1,ind+1):
                 return True
             sol[x][y]=0
             return False
 
-        sol=[[0]*col for i in range(row)]
-        for i in range(row):
-            for j in range(col):
-                if dfs(i,j,0):
-                    return True
-        else:
-            return False
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j]==word[0]:
+                    if find(i,j,0):
+                        return True
+        return False
